cb5bafbf3bc2cb67d607a4164f3dbffd636f646c
Amelia Bellamy-Royds <amelia.bellamy.royds@gmail.com>
2015-06-04 12:06:48 -0600

    Compress color runs, add error handling
    Also add a better test file.
    
    ___________________________
    
    Runs of same-color pixels are represented by a single rect element.
    
    If the image-parsing method fails, a message with a link to the file is
    printed to the page.
    
    The order of attributes has been shuffled to maximize the length of
    constant repeated strings.
    
    The unused code checking file extension has been removed.

--- converter.php
+++ converter.php
@@ -1,23 +1,37 @@
 <?php
 
-# $url = "http://area51/img2svg/test.png";
-$url = "http://fray.com/images/i3c.gif";
+# $url = "test.png";
+# $url = "test.gif";
+# $url = "http://fray.com/images/i3c.gif";
+# $url = "http://upload.wikimedia.org/wikipedia/commons/b/bb/Quilt_design_as_46x46_uncompressed_GIF.gif";
+  $url = "http://pbs.twimg.com/profile_images/539094743133614080/cj8fsBZE_reasonably_small.png";
 
 $img = loadImage($url);
 //var_dump( getimagesize($img));
-generateSVG($img);
+if (!!$img) { 
+  generateSVG($img); 
+}
+else {
+  echo "<a href=\"$url\">Bad image file</a>";
+}
 
 
 function generateSVG($img) {
 	$svg = '<svg>';
 	$w = imagesx($img); // image width
 	$h = imagesy($img); // image height
+  $n = 1; //number of consecutive pixels
 	for ($x = 0; $x < $w; $x++) {
-		for ($y = 0; $y < $h; $y++) {
+		for ($y = 0; $y < $h; $y = $y+$n) {
 			$col = imagecolorat($img, $x, $y);
-			$rgb = imagecolorsforindex($img, $col);
+      $n = 1;
+      while( ($y+$n < $h) &&
+        ($col == imagecolorat($img, $x, ($y+$n))) ) {
+        $n++;
+      }
+			$rgb = imagecolorsforindex($img, $col);      
 			$color = "rgb($rgb[red],$rgb[green],$rgb[blue])";
-			$svg .= "<rect x=\"$x\" y=\"$y\" width=\"1\" height=\"1\" fill=\"$color\" />\n";
+			$svg .= "<rect width=\"1\" x=\"$x\" height=\"$n\" y=\"$y\" fill=\"$color\"/>\n";
 		}
 	}
 	$svg .= '</svg>';
@@ -25,10 +39,6 @@ function generateSVG($img) {
 }
 
 function loadImage($url) {
-
-	$allowed = array('jpg','gif','png');
-	$pos = strrpos($url, ".");
-	$str = substr($url,($pos + 1));
 
 	$ch = curl_init();
 	$timeout = 0;
@@ -45,8 +55,7 @@ function loadImage($url) {
 
 	$img = @imagecreatefromstring($image);
 
-	return $img;
-
+  return $img; 
 }
 
 ?>
