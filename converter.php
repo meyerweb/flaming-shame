77e6205128f74d0730c8f36d443ce663a484fed5
Robin Cafolla <robin@zombiemongoose.com>
2015-06-05 09:09:01 +0200

    Added argument to phpdoc

--- converter.php
+++ converter.php
@@ -3,6 +3,8 @@
  * Flaming Shame Raster to SVG converter
  *
  * @author  Eric Meyers, Amelia Bellamy-Royds, Robin Cafolla
+ * @arg     string url      Takes a single string url or path to local image to
+ *                          convert from raster to SVG.
  */
 if (count($argv) < 1) {
     throw new \RuntimeException(
